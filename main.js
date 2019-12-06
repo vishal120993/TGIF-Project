2cd3bcd20e740d21ae484e5e9613e449df48528b
Vishal <vishalshende12@gmail.com>
2019-12-06 15:33:36 +0100

    seniority year added

--- main.js
+++ main.js
@@ -2,15 +2,18 @@
 const members = data.results[0].members;
 const republicanParty = document.getElementById("defaultInline1");
 const democratParty = document.getElementById("defaultInline2");
-const independentParty =  document.getElementById("defaultInline3");
-let membersState  = document.getElementById("stateFilter");
+const independentParty = document.getElementById("defaultInline3");
+const membersState = document.getElementById("stateFilter");
+const membersSeniority = document.getElementById("seniorityFilter");
+//let membersNames = document.getElementById("searchBar").value.toUpperCase();
+
 
 // Functions
 
 // function to create table
-function createTable(members,tableId) {
+function createTable(members, tableId) {
     let tablecontents = "";
- 
+
     tablecontents = "<table>";
     // Table headers
     tablecontents += "<tr>";
@@ -22,7 +25,7 @@ function createTable(members,tableId) {
     tablecontents += "</tr>";
 
     // For loop for looping through all the data 
-    
+
     for (i = 0; i < members.length; i++) {
         if (members[i].middle_name === null) {
             members[i].full_name = members[i].first_name + " " + members[i].last_name
@@ -42,90 +45,160 @@ function createTable(members,tableId) {
     tablecontents += "</table>";
     // linking the html tables  with javascript
     document.getElementById(tableId).innerHTML = tablecontents;
-      
+
 }
 
 // function to filter out members on the basis of party and state
-function filterMembers (members) {
-    let array  = [];
-     //console.log(democratParty);
-   for (let i = 0; i < members.length; i++) {
-    if (membersState.value == members[i].state || membersState.value =='all') {
-            if (members[i].party === "D" && democratParty.checked) {
-                array.push(members[i]);
-            } else if (members[i].party === "R" && republicanParty.checked) {
-                array.push(members[i]);
-            } else if (members[i].party === "I" && independentParty.checked) {
-                array.push(members[i]);
+function filterMembers(members) {
+    let array = [];
+    //console.log(democratParty);
+    for (let i = 0; i < members.length; i++) {
+        if (membersSeniority.value == members[i].seniority || membersSeniority.value == 'all') {
+            if (membersState.value == members[i].state || membersState.value == 'all') {
+                if (members[i].party === "D" && democratParty.checked) {
+                    array.push(members[i]);
+                } else if (members[i].party === "R" && republicanParty.checked) {
+                    array.push(members[i]);
+                } else if (members[i].party === "I" && independentParty.checked) {
+                    array.push(members[i]);
+                }
             }
         }
     }
-   //console.log(array);
-   return array;
-  }
+    //console.log(array);
+    return array;
+}
 
 // function to create the state options and sorting in ascending order
-function showState (members) {
+function showState(members) {
     let arrayOfState = [];
-    
+    let arrayOfSeniorityYear = [];
+
     for (let i = 0; i < members.length; i++) {
-        if (!arrayOfState.includes(members[i].state)) {
+        if (!arrayOfState.includes(members[i].state)) { // in total 50 states are there! state is pushed only once
             arrayOfState.push(members[i].state);
+        }
+
+        if (!arrayOfSeniorityYear.includes(Number(members[i].seniority))) { // seniority years are only pushed once
+            arrayOfSeniorityYear.push(Number(members[i].seniority));
         }
     }
 
-    arrayOfState.sort()
+    arrayOfState.sort() // alphabetically ordered states
 
     for (let i = 0; i < arrayOfState.length; i++) {
-        let option = document.createElement('option')
+        let option = document.createElement('option') // create options for states
         option.setAttribute('value', arrayOfState[i]);
-        option.innerHTML = arrayOfState[i];
-        membersState.appendChild(option)
+        option.innerHTML = arrayOfState[i]; // option are accessed as states
+        membersState.appendChild(option) // select is main, option is append child
+    }
+
+    arrayOfSeniorityYear.sort(function (a, b) {
+        return a - b;
+    });
+
+    for (let i = 0; i < arrayOfSeniorityYear.length; i++) {
+        let option = document.createElement('option') // create options for seniority years
+        option.setAttribute('value', arrayOfSeniorityYear[i]);
+        option.innerHTML = arrayOfSeniorityYear[i]; // option are accessed as seniority from 
+        membersSeniority.appendChild(option) // select is main, option is append child
     }
 }
+
 showState(members);
 
 
 // Calling the table
-
 createTable(members, "table-data");
 
+//Addeventlisteners for interactive checkbox and dropdown options
 democratParty.addEventListener("click", function () {
-    createTable(filterMembers(members),"table-data");
+    createTable(filterMembers(members), "table-data");
 });
 republicanParty.addEventListener("click", function () {
-    createTable(filterMembers(members),"table-data");
+    createTable(filterMembers(members), "table-data");
 });
-independentParty.addEventListener("click",  function () {
-    createTable(filterMembers(members),"table-data");
+independentParty.addEventListener("click", function () {
+    createTable(filterMembers(members), "table-data");
 });
- 
+
 membersState.addEventListener("change", function () {
     createTable(filterMembers(members), "table-data");
 });
 
+membersSeniority.addEventListener("change", function () {
+    createTable(filterMembers(members), "table-data");
+});
+
+/*
+membersNames.addEventListener("onkeyup", function () {
+    createTable(searchNames(members), "table-data");
+})
+*/
 
 
 
+// function for search bar
+/*
+function searchNames(members) {
+    let arrayOfNames = [];
+
+    for (let i = 0; i < members.length; i++) {
+        if (membersNames.value == members[i].full_name ) {
+            arrayOfNames.push(members[i]);
+        }
+    }
+    return arrayOfNames;
+}
+
+
+// array.filter()
+  let arrayOfCheckedBoxFilters = [];
+
+  function filterMembers (members) {
+      arrayOfCheckedBoxFilters.push(members[i].party === "D");
+  }
+arrayOfCheckedBoxFilters = arrayOfCheckedBoxFilters.filter(filterMembers);
+console.log(arrayOfCheckedBoxFilters);
+*/
 
 
 
+// function to create senority level filter
+/*function filterSeniorityMembers(members) {
+    let arrayOfSeniority = [];
+    //console.log(democratParty);
+    for (let i = 0; i < members.length; i++) {
+        if (membersSeniority.value == members[i].seniority || membersSeniority.value == 'all') {
+            arrayOfSeniority.push(members[i]);
+        }
+    }
+    //console.log(array);
+    return arrayOfSeniority;
+}
 
+// function to create the seniority options and sorting in ascending order
+function showSeniority(members) {
+    let arrayOfSeniorityYear = [];
 
+    for (let i = 0; i < members.length; i++) {
+        if (!arrayOfSeniorityYear.includes(members[i].seniority)) { // seniority years are only pushed once
+            arrayOfSeniorityYear.push(members[i].seniority);
+        }
+    }
 
+    //Create ascending order of seniority year
+    arrayOfSeniorityYear.sort((a, b) => Number(b.seniority) - Number(a.seniority));
 
+    for (let i = 0; i < arrayOfSeniorityYear.length; i++) {
+        let option = document.createElement('option') // create options for seniority years
+        option.setAttribute('value', arrayOfSeniorityYear[i]);
+        option.innerHTML = arrayOfSeniorityYear[i]; // option are accessed as seniority from 
+        membersSeniority.appendChild(option) // select is main, option is append child
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+showSeniority(members); */
 
 
 
